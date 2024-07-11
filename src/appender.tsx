ba0e4d007554640aa4035a3f1c6c868ccfd01241
Saccharin01 <chousik01@gmail.com>
2024-07-11 19:58:15 +0900

    backUp

--- src/appender.tsx
+++
@@ -1,8 +0,0 @@
-const appender = (parent : string, children: string)=>{
-  const container = document.querySelectorAll(`.${parent}`)
-  container.forEach((element)=>{
-    element.innerHTML = children
-  })
-}
-
-export default appender
