ede4b200a50a1cbc8653ffcba1b398641033df4f
Saccharin01 <chousik01@gmail.com>
2024-07-17 12:01:56 +0900

    축약 로직 정리

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const baseData  = require("./baseDataTest.js")
 
 const testFunc = ()=>{
    const cards = Array.isArray(baseData.h1.textNode) ? baseData.h1.textNode.length : 1
-   const testArray = Array.from({length : cards}, (_,index) =>`test`)
+   const testArray = Array.from({length : cards}, (_,index) =>{return index+1})
   console.log(testArray)
 }
 
